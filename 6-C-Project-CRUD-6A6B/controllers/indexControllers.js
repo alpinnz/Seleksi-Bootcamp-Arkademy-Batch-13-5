706586d78bd29dce41e2ea8decc905294fbe4e77
Alpinnz <alpinnz@gmail.com>
2019-11-23 19:35:38 +0700

    6-C-read

--- 6-C-Project-CRUD-6A6B/controllers/indexControllers.js
+++ 6-C-Project-CRUD-6A6B/controllers/indexControllers.js
@@ -5,17 +5,21 @@ const mysql = require('mysql')
 exports.read = async (req, res, next) => {
     let query = 'SELECT product.id AS id, cashier.name AS cashier, product.name AS product, category.name AS category, product.price AS price FROM((product INNER JOIN category ON product.id_category = category.id) INNER JOIN cashier ON product.id_cashier = cashier.id)'
     let items = await pool.query(query);
+    let itemsCashier = await pool.query('SELECT * FROM `cashier`')
+    let itemsCategory = await pool.query('SELECT * FROM `category`')
     res.render('index', {
-        dataTable: items
+        dataTable: items,
+        dataCashier: itemsCashier,
+        dataCategory: itemsCategory
     });
 };
 
 exports.add = async (req, res, next) => {
 
-    // let query = mysql.format('INSERT INTO  `product`(`id`, `name`, `price`, `id_category`, `id_cashier`) VALUES (?,?,?,?,?)',
-    //     [req.body.id, req.body.name, req.body.pice, req.body.id_category, req.body.id_cashier])
-    // let result = await pool.query(query);
-    // res.send(result);
+    let query = mysql.format('INSERT INTO  `product`(`id`, `name`, `price`, `id_category`, `id_cashier`) VALUES (?,?,?,?,?)',
+        [req.body.id, req.body.name, req.body.pice, req.body.id_category, req.body.id_cashier])
+    let result = await pool.query(query);
+    res.send(result);
 };
 
 exports.edit = async (req, res, next) => {};
