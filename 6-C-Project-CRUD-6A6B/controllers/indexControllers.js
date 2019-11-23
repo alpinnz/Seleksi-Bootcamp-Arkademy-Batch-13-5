e7af05cb6072945de2528daeab97498c849b4392
Alpinnz <alpinnz@gmail.com>
2019-11-23 19:13:15 +0700

    6-c

---
+++ 6-C-Project-CRUD-6A6B/controllers/indexControllers.js
@@ -0,0 +1,26 @@
+const pool = require('../database');
+const mysql = require('mysql')
+
+
+exports.read = async (req, res, next) => {
+    let query = 'SELECT product.id AS id, cashier.name AS cashier, product.name AS product, category.name AS category, product.price AS price FROM((product INNER JOIN category ON product.id_category = category.id) INNER JOIN cashier ON product.id_cashier = cashier.id)'
+    let items = await pool.query(query);
+    res.render('index', {
+        data: items
+    });
+    // res.send(items)
+};
+
+exports.add = async (req, res, next) => {
+
+    // let query = mysql.format('INSERT INTO  `product`(`id`, `name`, `price`, `id_category`, `id_cashier`) VALUES (?,?,?,?,?)',
+    //     [req.body.id, req.body.name, req.body.pice, req.body.id_category, req.body.id_cashier])
+    // let result = await pool.query(query);
+    // res.send(result);
+};
+
+exports.edit = async (req, res, next) => {};
+
+exports.delete = async (req, res, next) => {
+
+};
--- 6-C-Project-CRUD-6A6B/routes/index.js
+++ 6-C-Project-CRUD-6A6B/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const indexControllers = require('./../controllers/indexControllers')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('index', {
+//     title: 'Express'
+//   });
+// });
+
+router.get('/', indexControllers.read);
+router.post('/add', indexControllers.add);
+router.put('/edit', indexControllers.edit);
+router.delete('/delete', indexControllers.delete);
 
 module.exports = router;
