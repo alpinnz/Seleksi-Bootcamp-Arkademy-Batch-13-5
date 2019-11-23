963718e120d6af6279097793fb2d627c6e145179
Alpinnz <alpinnz@gmail.com>
2019-11-23 19:15:50 +0700

    6-c

--- 6-C-Project-CRUD-6A6B/controllers/indexControllers.js
+++ 6-C-Project-CRUD-6A6B/controllers/indexControllers.js
@@ -6,9 +6,8 @@ exports.read = async (req, res, next) => {
     let query = 'SELECT product.id AS id, cashier.name AS cashier, product.name AS product, category.name AS category, product.price AS price FROM((product INNER JOIN category ON product.id_category = category.id) INNER JOIN cashier ON product.id_cashier = cashier.id)'
     let items = await pool.query(query);
     res.render('index', {
-        data: items
+        dataTable: items
     });
-    // res.send(items)
 };
 
 exports.add = async (req, res, next) => {
